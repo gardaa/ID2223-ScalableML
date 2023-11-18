daa7e096a9a72f998d8d991818f9288440d1e1d4
Egill Friðriksson <egill@kth.se>
2023-11-18 16:40:08 +0100

    batch interference added

---
+++ wine-batch-inference-pipeline.py
@@ -0,0 +1,110 @@
+import os
+import modal
+    
+LOCAL=True
+
+if LOCAL == False:
+   stub = modal.Stub()
+   hopsworks_image = modal.Image.debian_slim().pip_install(["hopsworks","joblib","seaborn","sklearn==1.1.1","dataframe-image"])
+   @stub.function(image=hopsworks_image, schedule=modal.Period(days=1), secret=modal.Secret.from_name("HOPSWORKS_API_KEY"))
+   def f():
+       g()
+
+def g():
+    import pandas as pd
+    import hopsworks
+    import joblib
+    import datetime
+    from PIL import Image
+    from datetime import datetime
+    import dataframe_image as dfi
+    from sklearn.metrics import confusion_matrix
+    from matplotlib import pyplot
+    import seaborn as sns
+    import requests
+
+    project = hopsworks.login()
+    fs = project.get_feature_store()
+    
+    mr = project.get_model_registry()
+    model = mr.get_model("wine_model", version=1)
+    model_dir = model.download()
+    model = joblib.load(model_dir + "/wine_model.pkl")
+    
+    feature_view = fs.get_feature_view(name="wine", version=1)
+    batch_data = feature_view.get_batch_data()
+    
+    y_pred = model.predict(batch_data)
+    #print(y_pred)
+    offset = 1
+    quality = y_pred[y_pred.size-offset]
+    # flower_url = "https://raw.githubusercontent.com/featurestoreorg/serverless-ml-course/main/src/01-module/assets/" + flower + ".png"
+    wine_url = "https://raw.githubusercontent.com/gardaa/lab1-wine-quality/main/wine_images/" + str(quality) + ".jpg"
+
+    print("Wine predicted: " + str(quality))
+    img = Image.open(requests.get(wine_url, stream=True).raw)            
+    img.save("./latest_wine.jpg")
+    dataset_api = project.get_dataset_api()    
+    dataset_api.upload("./latest_wine.jpg", "Resources/images", overwrite=True)
+   
+    wine_fg = fs.get_feature_group(name="wine", version=1)
+    df = wine_fg.read() 
+    #print(df)
+    label = df.iloc[-offset]["quality"]
+    label_url = "https://raw.githubusercontent.com/gardaa/lab1-wine-quality/main/wine_images/" + str(int(label)) + ".jpg"
+    print("Actual quality: " + str(label))
+    img = Image.open(requests.get(label_url, stream=True).raw)            
+    img.save("./actual_quality.jpg")
+    dataset_api.upload("./actual_quality.jpg", "Resources/images", overwrite=True)
+    
+    monitor_fg = fs.get_or_create_feature_group(name="wine_predictions",
+                                                version=1,
+                                                primary_key=["datetime"],
+                                                description="Wine Prediction/Outcome Monitoring"
+                                                )
+    
+    now = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    data = {
+        'prediction': [quality],
+        'label': [label],
+        'datetime': [now],
+       }
+    monitor_df = pd.DataFrame(data)
+    monitor_fg.insert(monitor_df, write_options={"wait_for_job" : False})
+    
+    history_df = monitor_fg.read()
+    # Add our prediction to the history, as the history_df won't have it - 
+    # the insertion was done asynchronously, so it will take ~1 min to land on App
+    history_df = pd.concat([history_df, monitor_df])
+
+
+    df_recent = history_df.tail(4)
+    dfi.export(df_recent, './df_recent.png', table_conversion = 'matplotlib')
+    dataset_api.upload("./df_recent.png", "Resources/images", overwrite=True)
+    
+    predictions = history_df[['prediction']]
+    labels = history_df[['label']]
+
+    # Only create the confusion matrix when our iris_predictions feature group has examples of all 3 iris flowers
+    print("Number of different wine quality predictions to date: " + str(predictions.value_counts().count()))
+    if predictions.value_counts().count() == 3:
+        results = confusion_matrix(labels, predictions)
+    
+        df_cm = pd.DataFrame(results, range(3,9), range(3,9))
+    
+        cm = sns.heatmap(df_cm, annot=True)
+        fig = cm.get_figure()
+        fig.savefig("./confusion_matrix.png")
+        dataset_api.upload("./confusion_matrix.png", "Resources/images", overwrite=True)
+    else:
+        print("You need 6 different wine quality predictions to create the confusion matrix.")
+        print("Run the batch inference pipeline more times until you get 6 different wine quality predictions") 
+
+
+if __name__ == "__main__":
+    if LOCAL == True :
+        g()
+    else:
+        with stub.run():
+            f()
+
