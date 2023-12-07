623d16f840dc15eb60d7e78cd85aeec29ad7b2fe
Gard Aasness <gardaasness@gmail.com>
2023-12-07 13:13:38 +0100

    Create batch_inference.py for norwegian and change folder structure

---
+++ Lab2/Task_1/Norwegian/batch_inference.py
@@ -0,0 +1,18 @@
+from transformers import pipeline
+import gradio as gr
+
+pipe = pipeline(model="gardaa/whisper-small-norwegian")  # change to "your-username/the-name-you-picked"
+
+def transcribe(audio):
+    text = pipe(audio)["text"]
+    return text
+
+iface = gr.Interface(
+    fn=transcribe,
+    inputs=gr.Audio(sources=['microphone', 'upload'], type="filepath"),
+    outputs="text",
+    title="Whisper Small Norwegian",
+    description="Realtime demo for Norwegian speech recognition using a fine-tuned Whisper small model.",
+)
+
+iface.launch(share=True)
