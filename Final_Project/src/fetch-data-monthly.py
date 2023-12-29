a1e1f978f0ae87bf55f44b21e746cac17789cd32
Egill Friðriksson <egill@kth.se>
2023-12-29 17:28:34 +0000

    test

--- Final_Project/src/fetch-data-monthly.py
+++ Final_Project/src/fetch-data-monthly.py
@@ -3,8 +3,6 @@ import pandas as pd
 import requests
 from datetime import datetime
 from io import StringIO 
-import schedule
-import time
 
 def fetch_update_csv():
     # URL of the CSV file
@@ -21,7 +19,7 @@ def fetch_update_csv():
         current_dir = os.getcwd()
 
         # Save the DataFrame to a local CSV file in the GitHub repository
-        local_file_path = os.path.join(current_dir, 'kaupskra1.csv')
+        local_file_path = os.path.join(current_dir, 'kaupskra.csv')
         df.to_csv(local_file_path, index=False)
 
         print(f"CSV file updated successfully at {datetime.now()}")
@@ -29,10 +27,5 @@ def fetch_update_csv():
     else:
         print(f"Failed to fetch CSV data. Status code: {response.status_code}")
 
-# Schedule the task to run every 23rd of the month
-schedule.every().month.day.at("00:00").do(fetch_update_csv)
-
-# Keep the script running to execute scheduled tasks
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+# Execute the function once per run
+fetch_update_csv()
